eb7258bcf407017679bd2366506eab16c5a00084
Elle O'Brien <elleobrien11@gmail.com>
2020-10-30 18:05:20 -0700

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -10,4 +10,4 @@ ggplot(penguins, aes(bill_length_mm, bill_depth_mm))+
   geom_point(aes(colour=sex))+
   scale_color_viridis(discrete=TRUE)
 
-print("Anda pepperoni pizza or two or three")
+print("Anda pepperoni pizza or two or threeeeeee")
