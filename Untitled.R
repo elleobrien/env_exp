f5b2b46dd9b6ae04fec72481c050b71dfbe43712
Elle O'Brien <elleobrien11@gmail.com>
2020-10-30 17:31:35 -0700

    Update Untitled.R

--- Untitled.R
+++ Untitled.R
@@ -1,5 +1,2 @@
-options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/centos7/352"))
-install.packages("palmerpenguins")
 library(palmerpenguins)
-
-install.packages("ggplot2")
+head(penguins)
