43d7dca24bd5ccef715c05846ac8d2e8e1aeecc5
elleobrien <elleobrien11@gmail.com>
2020-10-30 17:24:36 -0700

    first commit

---
+++ .Rproj.user/CAE8C5D3/sources/s-DE110699/49E4119A-contents
@@ -0,0 +1,3 @@
+library(palmerpenguins)
+
+head(penguins)
---
+++ Untitled.R
@@ -0,0 +1,5 @@
+options(repos = c(REPO_NAME = "https://packagemanager.rstudio.com/all/__linux__/centos7/352"))
+install.packages("palmerpenguins")
+library(palmerpenguins)
+
+install.packages("ggplot2")
